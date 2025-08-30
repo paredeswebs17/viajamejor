60b6eda8dcfe03c9f004a5939bd40cd6bb9d98b8
paredeswebs17 <paredeswebs17@gmail.com>
2025-08-30 18:25:34 +0200

    Convert server.js to ES modules

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+import http from 'http';
+import fs from 'fs';
+import path from 'path';
 
 const server = http.createServer((req, res) => {
   let filePath = '.' + req.url;
