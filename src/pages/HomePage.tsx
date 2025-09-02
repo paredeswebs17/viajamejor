fd966e60da350f76820b7a5e2dbac9bdd4508def
paredeswebs17 <paredeswebs17@gmail.com>
2025-09-02 16:33:05 +0200

    Fix missing Link import in HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,5 @@
 import { Helmet } from 'react-helmet-async';
+import { Link } from 'react-router-dom';
 import Hero from '../components/Hero';
 import BlogSection from '../components/BlogSection';
 import About from '../components/About';
