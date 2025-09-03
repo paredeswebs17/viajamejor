3a6fdfd83d38034456cfadc54de43112d9c1e8bb
paredeswebs17 <paredeswebs17@gmail.com>
2025-09-03 03:22:26 +0200

    Fix Vite HMR WebSocket connection error

--- vite.config.ts
+++ vite.config.ts
@@ -29,10 +29,6 @@ export default defineConfig({
   server: {
     open: true,
     port: 5173,
-    host: true,
-    hmr: {
-      clientPort: 443,
-      host: 'zp1v56uxy8rdx5ypatb0ockcb9tr6a-oci3-eafxjfco--5173--96435430.local-corp.webcontainer-api.io'
-    }
+    host: true
   }
 })
