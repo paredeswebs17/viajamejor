6ef8867892504e98aa1677d80844f9833a9a9ae0
paredeswebs17 <paredeswebs17@gmail.com>
2025-10-12 11:30:39 +0200

    Updated AmsterdamGuideArticle.tsx

--- src/components/AmsterdamGuideArticle.tsx
+++ src/components/AmsterdamGuideArticle.tsx
@@ -67,7 +67,7 @@ Volver a guías
 
   <div className="mb-8">
     <img
-      src="https://www.pexels.com/es-es/foto/amsterdam-de-noche-16980404/"
+      src="https://images.pexels.com/photos/16980404/pexels-photo-16980404.jpeg"
       alt="Canales de Ámsterdam con casas tradicionales holandesas y bicicletas"
       className="w-full h-48 sm:h-64 lg:h-80 object-cover rounded-2xl shadow-lg"
     />
